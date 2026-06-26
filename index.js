445e082fbc1d52e120335f54bf2290baba5a16e2
Patrick Walshak <patrickwalshak1@gmail.com>
2026-06-26 22:53:43 +0100

    intial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
+const app = express(); 
 
 app.use(cors());
 app.use(express.json());
