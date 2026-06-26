d1fca32284ef7d7aadacde42edc6b030952d85ae
Patrick Walshak <patrickwalshak1@gmail.com>
2026-06-26 22:51:16 +0100

    Add dotenv and cors setup

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, my server is working!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
