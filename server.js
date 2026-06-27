53fc296b57d20701e38c9b3c2a2a2254a705d59c
Patrick Walshak <patrickwalshak1@gmail.com>
2026-06-27 14:15:37 +0100

    intial commit

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
+const sequelize = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("Database connection failed:", err));
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
