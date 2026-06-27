762e35b50de95927ca73e419fe9d701318c30d03
Patrick Walshak <patrickwalshak1@gmail.com>
2026-06-27 13:48:39 +0100

    created express folder structure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const app = express(); 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +11,4 @@ app.get("/", (req, res) => {
   res.send("Hello, my server is working!");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
