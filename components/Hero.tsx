e97a219b400843c456972126893c35fe16b9598e
Patrick Walshak <patrickwalshak1@gmail.com>
2026-06-28 15:50:11 +0100

    initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,32 @@
 export default function Hero() {
   return (
     <section
-      className="relative h-[600px] flex items-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/hero.jpg')" }}
+      className="relative min-h-screen flex items-center bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://plus.unsplash.com/premium_photo-1664202525979-80d1da46b34b?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      }}
     >
-      {/* dark overlay so text stays readable */}
-      <div className="absolute inset-0 bg-espresso/40" />
+        {/* espresso */}
+      <div className="absolute inset-0 bg-black/60" />
 
-      <div className="relative z-10 px-8 md:px-16 max-w-lg">
-        <p className="text-cream text-sm uppercase tracking-widest mb-3">
-          Fall Essentials
-        </p>
-        <h2 className="text-cream text-4xl md:text-5xl font-bold mb-6 leading-tight">
-          Update Your Wardrobe.
-          <br />
-          Shop the New Collection
-        </h2>
+        <div className="relative z-10 px-12 mx-auto text-center">
+            <p className="text-white text-2xl font-extrabold uppercase tracking-widest mb-3">
+              Fall Essentials
+            </p>
+            {/* <h2 className="text-cream text-xl md:text-5xl font-bold mb-6 text-center">
+              Quality clothing crafted with modern design and made for every occasion.
+            </h2> */}
 
-        <div className="flex gap-4">
-          
-            <a href="/products?category=men" className="bg-camel text-espresso px-6 py-3 rounded-md font-medium hover:bg-cocoa hover:text-cream transition">
-            Shop Men
-          </a>
-          
-           <a href="/products?category=women" className="bg-camel text-espresso px-6 py-3 rounded-md font-medium hover:bg-cocoa hover:text-cream transition">
-            Shop Women
-          </a>
-        </div>
+            <div className="text-[50px] font-serif font-extrabold text-white">
+                <p>Quality clothing crafted with <span className="text-[#A78D78]"> modern design<br></br> and made</span> for every occasion.</p>
+            </div>
+
+            <div className="flex gap-4 justify-center">
+                <a href="/products?category=men"className="bg-[#6E473B] text-white px-6 py-3 rounded-md font-bold hover:bg-cocoa hover:text-cream transition">Shop Men</a>
+
+                <a href="/products?category=women"className="bg-[#6E473B] text-white px-6 py-3 rounded-md font-bold hover:bg-cocoa hover:text-cream transition">Shop Women</a>
+            </div>
       </div>
     </section>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,28 +2,29 @@ import { Search, User, Heart, ShoppingCart } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-12 py-4 bg-cream">
-      <h1 className="text-xl font-bold tracking-wide text-espresso">
-        NOVAFIT
-      </h1>
+    <nav className="px-12 py-4 bg-cream font-serif">
+        <div className="flex items-center justify-between mx-32">
+        <h1 className="text-4xl font-extrabold tracking-wide text-espresso">
+            NOVAFIT
+        </h1>
 
-      <div className="hidden md:flex gap-8 text-sm font-medium text-espresso">
-        <a href="/" className="border-b-2 border-espresso pb-1">Home</a>
-        <a href="/products">Shop</a>
-        <a href="/products?sort=new">New Arrivals</a>
-        <a href="/products?category=men">Men</a>
-        <a href="/products?category=women">Women</a>
-        <a href="/products?category=accessories">Accessories</a>
-      </div>
+        <div className="hidden md:flex gap-8 text-xl font-extrabold text-espresso">
+            <a href="/" className="border-b-2 border-espresso pb-1">Home</a>
+            <a href="/products">Shop</a>
+            <a href="/products?sort=new">New Arrivals</a>
+            <a href="/products?category=men">Men</a>
+            <a href="/products?category=women">Women</a>
+            <a href="/products?category=accessories">Accessories</a>
+        </div>
 
-      <div className="flex gap-5 items-center text-espresso">
-        <Search size={18} className="cursor-pointer" />
-        <a href="/login" className="text-sm font-medium">Login</a>
-        <a href="/register" className="text-sm font-medium">Register</a>
-        <Heart size={18} className="cursor-pointer" />
-        <a href="/cart">
-          <ShoppingCart size={18} className="cursor-pointer" />
-        </a>
+        <div className="flex gap-5 items-center text-espresso">
+            <Search size={30} className="cursor-pointer" />
+            <a href="/login" className="text-sm  bg-[#6E473B] text-white px-6 py-3 rounded-md font-bold">Login</a>
+            <a href="/register" className="text-sm  bg-[#6E473B] text-white px-6 py-3 rounded-md font-bold">Register</a>
+            <a href="/cart">
+            <ShoppingCart size={30} className="cursor-pointer" />
+            </a>
+        </div>
       </div>
     </nav>
   );
